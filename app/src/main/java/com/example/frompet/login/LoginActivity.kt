8e77c6e15290038c68dc9849ecef7a403244b4b2
4seokhyeon <tktjrgus@naver.com>
2023-10-12 11:02:39 +0900

    [RENAME] : 파일 위치 변경 main, login

--- app/src/main/java/com/example/frompet/login/LoginActivity.kt
+++ app/src/main/java/com/example/frompet/login/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.frompet
+package com.example.frompet.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.frompet.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
