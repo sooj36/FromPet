d4db70c902e0ab50720d7351a61c7c95fb478557
4seokhyeon <tktjrgus@naver.com>
2023-10-12 18:41:01 +0900

    [FEAT]: 로그인 및 회원가입 뷰모델 적용 완료 Todo: 로그인별 예외처리🤮

--- app/src/main/java/com/example/frompet/login/SingUpActivity.kt
+++ app/src/main/java/com/example/frompet/login/SingUpActivity.kt
@@ -1,9 +1,13 @@
 package com.example.frompet.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.frompet.R
 import com.example.frompet.databinding.ActivitySingUpBinding
+import com.example.frompet.login.viewmodel.LoginViewModel
+import com.example.frompet.login.viewmodel.LoginViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -11,12 +15,24 @@ import com.google.firebase.ktx.Firebase
 
 class SingUpActivity : AppCompatActivity() {
     private lateinit var mBinding:ActivitySingUpBinding
-    private lateinit var mAuth: FirebaseAuth
+    private val viewModel by lazy {
+        ViewModelProvider(this, LoginViewModelFactory())[LoginViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivitySingUpBinding.inflate(layoutInflater)
-        mAuth = FirebaseAuth.getInstance()
         setContentView(mBinding.root)
+
+        mBinding.SignUpButton.setOnClickListener {
+            val email = mBinding.etEmailUp.text.toString()
+            val password = mBinding.etPasswordUp.text.toString()
+            viewModel.signUp(email,password)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
     }
 }
