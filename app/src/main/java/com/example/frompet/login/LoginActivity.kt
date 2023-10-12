f1182059b2163f87954a59e9154076f93e8831d3
4seokhyeon <tktjrgus@naver.com>
2023-10-12 18:41:01 +0900

    [FEAT]: 로그인 및 회원가입 뷰모델 적용 완료 Todo: 로그인별 예외처리🤮

--- app/src/main/java/com/example/frompet/login/LoginActivity.kt
+++ app/src/main/java/com/example/frompet/login/LoginActivity.kt
@@ -1,16 +1,53 @@
 package com.example.frompet.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.frompet.R
 import com.example.frompet.databinding.ActivityLoginBinding
 import com.example.frompet.databinding.ActivityMainBinding
+import com.example.frompet.home.HomeFragment
+import com.example.frompet.login.viewmodel.LoginViewModel
+import com.example.frompet.login.viewmodel.LoginViewModelFactory
+import com.example.frompet.main.MainActivity
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mBinding:ActivityLoginBinding
+    private val viewModel by lazy {
+        ViewModelProvider(this,
+            LoginViewModelFactory()
+        )[LoginViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         mBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mBinding.btLogin.setOnClickListener {
+            val email = mBinding.etEmail.text.toString()
+            val password = mBinding.etPassword.text.toString()
+            viewModel.singIn(email,password)
+
+        }
+
+        mBinding.btSingup.setOnClickListener {
+            val intent = Intent(this, SingUpActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        viewModel.loginResult.observe(this) { loginSuccess ->
+            if (loginSuccess) {
+                // 로그인 성공한 경우
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // 로그인 실패한 경우, 처리를 하지 않음
+                // 실패 메시지 표시나 다른 작업을 수행하려면 여기에 추가
+            }
+        }
     }
-}
+    }
--- app/src/main/java/com/example/frompet/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/frompet/login/viewmodel/LoginViewModel.kt
@@ -1,29 +1,61 @@
 package com.example.frompet.login.viewmodel
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.frompet.login.data.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import java.lang.IllegalArgumentException
 
 class LoginViewModel : ViewModel() {
     private val auth = FirebaseAuth.getInstance()
     private val _user = MutableLiveData<FirebaseUser?>()
     val user: LiveData<FirebaseUser?> = _user
 
-    fun singIn(email:String,password:String){
+    private val _loginResult = MutableLiveData<Boolean>()
+    val loginResult: LiveData<Boolean> = _loginResult
+
+
+    fun singIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if(task.isSuccessful){
+                if (task.isSuccessful) {
                     _user.value = auth.currentUser
-                }else{
+                    _loginResult.value = true
+                } else {
+                    Log.e("zzzzzzzz", "로그인 실패: ${task.exception}")
                     _user.value = null
+                    _loginResult.value = false
                 }
             }
     }
 
-    fun signUp(){
-
+    fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _user.value = auth.currentUser
+                } else {
+                    Log.e("zzzzzzzz", "회원가입 실패: ${task.exception}")
+                    _user.value = null
+                }
+            }
     }
 }
+
+    class LoginViewModelFactory(
+
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+                return LoginViewModel(
+                ) as T
+            } else {
+                throw IllegalArgumentException("Not found ViewModel class")
+            }
+        }
+    }
