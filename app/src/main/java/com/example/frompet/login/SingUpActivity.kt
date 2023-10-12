71e9d74d75cc6acbd5ec8fb643ea7be2d4733ad1
4seokhyeon <tktjrgus@naver.com>
2023-10-12 16:22:36 +0900

    [FEAT]: 로그인 및 회원가입 뷰모델 설계중

--- app/src/main/java/com/example/frompet/login/SingUpActivity.kt
+++ app/src/main/java/com/example/frompet/login/SingUpActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.frompet.R
 import com.example.frompet.databinding.ActivitySingUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class SingUpActivity : AppCompatActivity() {
     private lateinit var mBinding:ActivitySingUpBinding
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivitySingUpBinding.inflate(layoutInflater)
+        mAuth = FirebaseAuth.getInstance()
         setContentView(mBinding.root)
     }
 }
