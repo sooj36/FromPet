b8b7e8afd68474f7b8b0dc4b55012ce9863d60ec
bora park <jemini9595@gmail.com>
2023-10-14 03:29:24 +0900

    [Fix] : 뷰페이저 채팅프래그먼트에 채팅홈,채팅리스트프래그먼트 설정,닷츠인디케이터

--- app/src/main/java/com/example/frompet/chating/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/frompet/chating/adapter/ViewPagerAdapter.kt
@@ -3,7 +3,7 @@ package com.example.frompet.chating.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.frompet.chating.ChatFragment
-import com.example.frompet.chating.ChatHomeFragment
+import com.example.frompet.chating.ChatMessageFragment
 import com.example.frompet.chating.ChatListFragment
 
 class ViewPagerAdapter(private val fragmentActivity: ChatFragment, private val fragments:List<Fragment>):FragmentStateAdapter(fragmentActivity) {
@@ -13,9 +13,9 @@ class ViewPagerAdapter(private val fragmentActivity: ChatFragment, private val f
 
     override fun createFragment(position: Int): Fragment {
        return when(position){
-           0 ->return ChatHomeFragment()
+           0 ->return ChatMessageFragment()
            1 ->return ChatListFragment()
-           else ->return ChatHomeFragment()
+           else ->return ChatMessageFragment()
        }
     }
 
