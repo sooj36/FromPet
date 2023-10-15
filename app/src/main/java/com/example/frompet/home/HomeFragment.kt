bcce16c54341ad2a4ced4840a35d1db9b0fe9f6b
bora park <jemini9595@gmail.com>
2023-10-16 01:13:05 +0900

    [Feat] : 더미데이터(지울예정)

--- app/src/main/java/com/example/frompet/home/HomeFragment.kt
+++ app/src/main/java/com/example/frompet/home/HomeFragment.kt
@@ -5,28 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.frompet.R
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.example.frompet.databinding.FragmentHomeBinding
+import com.example.frompet.login.viewmodel.MatchViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
 
-    private var _binding : FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: MatchViewModel by viewModels()
+    private val auth = FirebaseAuth.getInstance()
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-         _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
 
+        binding.likeBtn.setOnClickListener {
+            val currentUid = auth.currentUser?.uid?:return@setOnClickListener
+            viewModel.like("Td4QjX4JQ2Y0EzUVmkY2JIYz8ML2")
+            Toast.makeText(requireContext(),"하트뿅뿅",Toast.LENGTH_LONG).show()
+        }
+
+        binding.dislikeBtn.setOnClickListener {  }
 
         return binding.root
     }
+
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
-
 }
