f25b7e7594ebe7ed9cb73577c09abc99b09824b6
4seokhyeon <tktjrgus@naver.com>
2023-10-16 01:39:52 +0900

    [FEAT]:Dagger2 Hilt 적용 비밀번호 찾기 기능만 사용

---
+++ app/src/main/java/com/example/frompet/login/repository/BaseAuthRepository.kt
@@ -0,0 +1,16 @@
+package com.example.frompet.login.repository
+
+import com.google.firebase.auth.FirebaseUser
+
+interface BaseAuthRepository {
+
+    suspend fun signInWithEmailPassword(email:String , password:String): FirebaseUser?
+
+    suspend fun signUpWithEmailPassword(email: String , password: String): FirebaseUser?
+
+    fun signOut() : FirebaseUser?
+
+    fun getCurrentUser() : FirebaseUser?
+
+    suspend fun sendResetPassword(email : String) : Boolean
+}
