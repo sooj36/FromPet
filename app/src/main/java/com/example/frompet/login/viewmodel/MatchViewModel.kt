6183bd943fac40c20fdde03236e51b2671cfd96a
bora park <jemini9595@gmail.com>
2023-10-16 20:41:44 +0900

    [Fix] : matchViewModel 필요없는거 간단한정리

--- app/src/main/java/com/example/frompet/login/viewmodel/MatchViewModel.kt
+++ app/src/main/java/com/example/frompet/login/viewmodel/MatchViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.frompet.login.viewmodel
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.frompet.login.data.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -22,17 +24,16 @@ class MatchViewModel : ViewModel() {
 
     fun like(targetUserId: String) {
         val currentUserId = auth.currentUser?.uid ?: return
-        Log.d("jun", "Current User ID: $currentUserId, Target User ID: $targetUserId")
-        database.child(currentUserId).child("liked").child(targetUserId).setValue(true)
+        Log.d("jun", "현재유저 ID: $currentUserId, 타겟유저 ID: $targetUserId")
         database.child(targetUserId).child("likedBy").child(currentUserId).setValue(true)
-        MatchedUser(targetUserId)
+
     }
 
     fun dislike(targetUserId: String) {
         val currentUserId = auth.currentUser?.uid ?: return
-        database.child(currentUserId).child("disliked").child(targetUserId).setValue(true)
         database.child(targetUserId).child("dislikedBy").child(currentUserId).setValue(true)
     }
+
     fun loadlikes() {
         val currentUserId = auth.currentUser?.uid ?: return
 
@@ -43,7 +44,6 @@ class MatchViewModel : ViewModel() {
                 val likedUsers = mutableListOf<UserModel>()
 
                 likedUserIds.forEach { userId ->
-
                     database.child(currentUserId).child("matched").child(userId).addListenerForSingleValueEvent(object: ValueEventListener {
                         override fun onDataChange(matchedSnapshot: DataSnapshot) {
                             if (!matchedSnapshot.exists()) { // 매치된 사용자가 아닌 경우에만 추가
@@ -66,41 +66,22 @@ class MatchViewModel : ViewModel() {
             override fun onCancelled(error: DatabaseError) {}
         })
     }
-    private fun MatchedUser(otherUserUid: String) {
+
+    fun matchWithUser(otherUserUid: String) {
         val currentUserId = auth.currentUser?.uid ?: return
 
-        val otherUserLikedByMe = database.child(currentUserId).child("liked").child(otherUserUid)
-        val meLikedByOtherUser = database.child(otherUserUid).child("liked").child(currentUserId)
-
-        otherUserLikedByMe.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.value == true) {
-                    meLikedByOtherUser.addListenerForSingleValueEvent(object : ValueEventListener {
-                        override fun onDataChange(snapshot: DataSnapshot) {
-                            if (snapshot.value == true) {
-                                // 서로 like한 경우
-                                Log.d("jun", "매치 유저 uid: $otherUserUid")
-                                database.child(currentUserId).child("matched").child(otherUserUid).setValue(true)
-                                database.child(otherUserUid).child("matched").child(currentUserId).setValue(true)
-                                val currentLikes = likeList.value?.toMutableList()
-                                currentLikes?.removeIf { it.uid == otherUserUid }
-                                likeList.value =currentLikes
-                                Log.d("jun","매치된후라이크리스트:${likeList.value}")
-
-//                                loadlikes()
-                            }
-                        }
-                        override fun onCancelled(error: DatabaseError) {
-                            Log.d("jun", "Error: ${error.message}")
-                        }
-                    })
-                } else {
-                    Log.d("jun", "Not matched user: $otherUserUid")
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {}
-        })
+        // 서로 like한 경우
+        Log.d("jun", "매치 유저 uid: $otherUserUid")
+        database.child(currentUserId).child("matched").child(otherUserUid).setValue(true)
+        database.child(otherUserUid).child("matched").child(currentUserId).setValue(true)
+        database.child(currentUserId).child("likedBy").child(otherUserUid).removeValue()
+        val currentLikes = likeList.value?.toMutableList()?: mutableListOf()
+        currentLikes?.removeIf { it.uid == otherUserUid }
+        likeList.value = currentLikes
+        Log.d("jun","매치된후라이크리스트:${likeList.value}")
+        loadlikes()
     }
+
 
     fun loadMatchedUsers() {
         val currentUserId = auth.currentUser?.uid ?: return
@@ -129,12 +110,10 @@ class MatchViewModel : ViewModel() {
             override fun onCancelled(error: DatabaseError) {}
         })
     }
-
-
-
-
-
 }
+
+
+
 
 
 
