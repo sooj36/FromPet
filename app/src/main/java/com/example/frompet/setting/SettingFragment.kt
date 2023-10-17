d21332750cc5dc64fd97ecda7cbd7a5eb8f9c2b0
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-17 21:17:56 +0900

    [DESIGN]  설정 프래그먼트

--- app/src/main/java/com/example/frompet/setting/SettingFragment.kt
+++ app/src/main/java/com/example/frompet/setting/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.frompet.setting
 
+import SettingViewModel
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -22,34 +23,51 @@ class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: LoginViewModel by viewModels()
+    private val viewModel: SettingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
 
-        viewModel.loadUserPetProfile { petProfile, petName, petType ->
+        viewModel.loadUserPetProfile()
+
+        viewModel.petProfile.observe(viewLifecycleOwner) { petProfile ->
             petProfile?.let {
                 Glide.with(this)
                     .load(it)
                     .into(binding.ivPet)
             }
+        }
+
+        viewModel.petName.observe(viewLifecycleOwner) { petName ->
             binding.tvPetName.text = petName
+        }
+
+        viewModel.petType.observe(viewLifecycleOwner) { petType ->
             binding.tvPetType.text = petType
         }
+
+
         binding.ibLogOut.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(requireActivity(), LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
         }
+
         binding.btModify.setOnClickListener {
             val intent = Intent(requireActivity(), ProfileActivity::class.java)
             startActivity(intent)
         }
+
         return binding.root
+    }
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadUserPetProfile()
     }
 
     override fun onDestroyView() {
