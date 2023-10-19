e267eaea897745e314820075e4fa93c943ee73e6
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-19 10:15:34 +0900

    [DESIGN]  채팅 화면 꾸미기

--- app/src/main/java/com/example/frompet/setting/SettingFragment.kt
+++ app/src/main/java/com/example/frompet/setting/SettingFragment.kt
@@ -27,6 +27,11 @@ class SettingFragment : Fragment() {
 
     private val viewModel: SettingViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.loadUserPetProfile()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,7 +39,6 @@ class SettingFragment : Fragment() {
 
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
 
-        viewModel.loadUserPetProfile()
 
         viewModel.petProfile.observe(viewLifecycleOwner) { petProfile ->
             petProfile?.let {
