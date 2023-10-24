5da449b0e6199d0c2a92b9c002ae127ab69e6e5c
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-25 00:47:19 +0900

    [FEAT] 친구목록 보이기

--- app/src/main/java/com/example/frompet/ui/setting/FriendsListActivity.kt
+++ app/src/main/java/com/example/frompet/ui/setting/FriendsListActivity.kt
@@ -2,17 +2,40 @@ package com.example.frompet.ui.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.frompet.MatchSharedViewModel
+import com.example.frompet.R
+import com.example.frompet.data.model.User
 import com.example.frompet.databinding.ActivityFriendsListBinding
-
-
 class FriendsListActivity : AppCompatActivity() {
-    private var _binding: ActivityFriendsListBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityFriendsListBinding // 바인딩 변수 선언
+
+    private val matchSharedViewModel: MatchSharedViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FriendsListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityFriendsListBinding.inflate(layoutInflater)
+        binding = ActivityFriendsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        recyclerView = binding.rvFriends
+        adapter = FriendsListAdapter(this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        matchSharedViewModel.matchedList.observe(this) { users ->
+            adapter.submitList(users)
+        }
+        matchSharedViewModel.loadMatchedUsers()
+
+        binding.btBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/frompet/ui/setting/FriendsListAdapter.kt
+++ app/src/main/java/com/example/frompet/ui/setting/FriendsListAdapter.kt
@@ -1,4 +1,54 @@
+// FriendsListAdapter.kt
+
 package com.example.frompet.ui.setting
 
-class FriendsListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.frompet.R
+import com.example.frompet.data.model.User
+import com.example.frompet.databinding.ItemChatlistBinding
+import com.example.frompet.databinding.ItemFriendsBinding
+import com.example.frompet.ui.chat.adapter.ChatListAdapter
+
+class FriendsListAdapter(private val context: Context) :
+    ListAdapter<User, FriendsListAdapter.ViewHolder>(DiffCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val binding = ItemFriendsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bindItems(user)
+    }
+
+    class ViewHolder(private val binding: ItemFriendsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindItems(user: User) {
+            binding.apply {
+                tvUserName3.text = user.petName
+                tvUserType3.text = user.petType
+                Glide.with(root.context).load(user.petProfile).into(binding.profileArea3)
+
+            }
+        }
+    }
+    class DiffCallback : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.uid == newItem.uid
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+    }
+
 }
