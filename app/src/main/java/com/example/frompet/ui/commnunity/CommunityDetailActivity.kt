9f92cfede5f36ec339f726f5c8c97fe1eef89528
sooj36 <sooj2023@gmail.com>
2023-10-24 16:14:52 +0900

    [UI] 커뮤니티 애드 기본화면 구현했습니다.

--- app/src/main/java/com/example/frompet/ui/commnunity/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/frompet/ui/commnunity/CommunityDetailActivity.kt
@@ -1,12 +1,28 @@
 package com.example.frompet.ui.commnunity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.frompet.R
+import com.example.frompet.databinding.ActivityCommunityBinding
+import com.example.frompet.databinding.ActivityCommunityDetailBinding
+import com.example.frompet.ui.commnunity.community.CommunityActivity
 
 class CommunityDetailActivity : AppCompatActivity() {
+
+    private var _binding : ActivityCommunityDetailBinding? = null
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community_detail)
+
+        _binding = ActivityCommunityDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        binding?.ivDetailBackbtn?.setOnClickListener {
+            val inent : Intent = Intent(this, CommunityActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
