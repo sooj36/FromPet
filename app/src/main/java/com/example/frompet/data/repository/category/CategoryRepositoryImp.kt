c7e6be541068acd29215cd007723e4a286f16042
4seokhyeon <tktjrgus@naver.com>
2023-11-03 21:33:31 +0900

    [fix] Imp 구현체 petType 필터 로직 추가했어요

--- app/src/main/java/com/example/frompet/data/repository/category/CategoryRepositoryImp.kt
+++ app/src/main/java/com/example/frompet/data/repository/category/CategoryRepositoryImp.kt
@@ -6,6 +6,7 @@ import com.example.frompet.R
 import com.example.frompet.data.model.CommunityData
 import com.example.frompet.data.model.CommunityHomeData
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class CategoryRepositoryImp(
@@ -37,22 +38,40 @@ class CategoryRepositoryImp(
         val communityDataList = mutableListOf<CommunityData>()
         try {
             val querySnapshot = firestore.collection("Community")
-                .whereEqualTo("petType",petType).get().await()
-            Log.e("aaaaaa","$petType")
-            for(document in querySnapshot.documents){
-                val petType = document.getString("petType") ?: ""
+                .whereEqualTo("petType",petType)
+                .orderBy("timestamp",Query.Direction.DESCENDING)
+                .get()
+                .await()
+            Log.e("aaaaaa", "$petType")
+            for (document in querySnapshot.documents) {
+                val title = document.getString("title") ?: ""
+                val tag = document.getString("tag") ?: ""
+                val timestamp = document.getString("timestamp") ?: ""
+                val contents = document.getString("contents") ?: ""
+                val uid = document.getString("uid") ?: ""
                 val docsId = document.id
+                val petType = document.getString("petType") ?: ""
 
-                val communityData = CommunityData(petType = petType, docsId = docsId)
+                val communityData = CommunityData(
+                    title = title,
+                    tag = tag,
+                    timestamp = timestamp,
+                    contents = contents,
+                    uid = uid,
+                    docsId = docsId,
+                    petType = petType
+                )
                 communityDataList.add(communityData)
             }
-            Log.e("aaaaa","$communityDataList")
+            Log.e("aaaaa", "$communityDataList")
 
-        }catch (e:Exception){
-            Log.e("aaaaaa","error",e)
+        } catch (e: Exception) {
+            Log.e("aaaaaa", "error", e)
         }
         return communityDataList
+
     }
+
 
     private fun getAnimalImage(categoryString: String): Int {
         val resourceName = categoryString.split("_").last()
