6533df8917d453c70893b4013cfc80a27dce3cc1
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-11-10 20:51:19 +0900

    [Feat] 댓글에서 대댓글 기능 추가 (아직 미완성)

--- app/src/main/java/com/pet/frompet/data/model/ReCommentData.kt
+++ app/src/main/java/com/pet/frompet/data/model/ReCommentData.kt
@@ -11,7 +11,7 @@ data class ReCommentData(
     val authorUid: String = "",
     val timestamp: Long = System.currentTimeMillis(),
     val reportCount: Int = 0,
-    val likeCount: Int = 0,
+    val likeCount: Int = 0,//뭐야!
     val likeUsers: List<String> = listOf()
 ): Parcelable {
     constructor() : this("", "", "", "", 0,0,0,listOf()) // 기본 생성자 추가
