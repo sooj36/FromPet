c3ecbbb4c7f97d190755193158271353ec880e64
sooj <sooj2023@gmail.com>
2023-11-13 21:06:12 +0900

    [FIX] 다른 사용자 마커  viewmodel로 이동

--- app/src/main/java/com/pet/frompet/ui/map/MapViewModel.kt
+++ app/src/main/java/com/pet/frompet/ui/map/MapViewModel.kt
@@ -4,21 +4,33 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.pet.frompet.data.model.UserLocation
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
+import com.pet.frompet.data.model.User
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MapViewModel() : ViewModel() {
 
     private val database = Firebase.database
+    private val firestore = FirebaseFirestore.getInstance()
     private val locationRef = database.getReference("location")
     private val currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: "" // 현재 uid 갖고 옴
 
     private val _userLocationInfo = MutableLiveData<UserLocationInfo>()
     val userLocation : LiveData<UserLocationInfo> get() = _userLocationInfo
+
+    private val _otherUserLocation = MutableLiveData<List<UserLocationInfo>>()
+    val otherUserLocation : MutableLiveData<List<UserLocationInfo>> get() = _otherUserLocation
 
     // 상태 클래스 (상태패턴)
     data class UserLocationInfo(
@@ -65,5 +77,25 @@ class MapViewModel() : ViewModel() {
         val userLocation = UserLocation(latitude, longitude)
         locationRef.child(currentUserId).setValue(userLocation)
 
+    }
+
+    // 사용자 위치 마커 표시
+    fun getOtherUserLocations() {
+        locationRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userLocationList = mutableListOf<UserLocationInfo>()
+                for (snapshots in snapshot.children) {
+                    val location = snapshots.getValue(UserLocation::class.java)
+                    val userUid = snapshots.key
+
+                    if (location != null && userUid != null && userUid != currentUserId) {
+                        userLocationList.add(UserLocationInfo(listOf(userUid), listOf(location)))
+                    }
+                }
+                _otherUserLocation.value = userLocationList
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 }
